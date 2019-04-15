d099c0b57903ccfd35e20876ab4029827cc19b4f
gimena87 <gimena.ramos87@gmail.com>
2019-04-14 23:41:01 -0300

    Add files via upload

--- android/app/src/main/java/com/proyectofinal/MainApplication.java
+++ android/app/src/main/java/com/proyectofinal/MainApplication.java
@@ -1,13 +1,24 @@
 package com.proyectofinal;
 
 import android.app.Application;
-
+import com.agontuk.RNFusedLocation.RNFusedLocationPackage;
 import com.facebook.react.ReactApplication;
+import com.imagepicker.ImagePickerPackage;
+import com.rnfs.RNFSPackage;
+import fr.bamlab.rnimageresizer.ImageResizerPackage;
+import com.ocetnik.timer.BackgroundTimerPackage;
+import com.oblador.vectoricons.VectorIconsPackage;
+import com.airbnb.android.react.maps.MapsPackage;
+import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
-
+import io.invertase.firebase.RNFirebasePackage;
+import  io.invertase.firebase.database.RNFirebaseDatabasePackage;
+import io.invertase.firebase.firestore.RNFirebaseFirestorePackage;
+import  io.invertase.firebase.auth.RNFirebaseAuthPackage;
+import com.RNFetchBlob.RNFetchBlobPackage;
 import java.util.Arrays;
 import java.util.List;
 
@@ -22,7 +33,20 @@ public class MainApplication extends Application implements ReactApplication {
     @Override
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
-          new MainReactPackage()
+          new MainReactPackage(),
+            new ImagePickerPackage(),
+            new RNFSPackage(),
+            new ImageResizerPackage(),
+            new BackgroundTimerPackage(),
+            new VectorIconsPackage(),
+            new MapsPackage(),
+            new RNFirebasePackage(),
+            new RNFirebaseDatabasePackage(),
+            new RNFirebaseAuthPackage(),
+            new RNFirebaseFirestorePackage(),
+            new RNGestureHandlerPackage(),
+            new RNFetchBlobPackage(),
+            new RNFusedLocationPackage()
       );
     }
 
